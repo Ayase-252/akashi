48eb147419417e4c7f31052aa5194ee4c2c5a3c0
Qingyu Deng <bitdqy@hotmail.com>
2017-01-05 15:32:40 +0800

    add parse function to parser class

--- src/parser.hpp
+++ src/parser.hpp
@@ -2,15 +2,16 @@
 #define PARSER_HPP
 
 #include "json.hpp"
+#include <string>
 
 using std::string;
 using nlohmann::json;
 
 namespace akashi {
-  class parser {
-  public:
-    static json parse(string str);
-  };
+class parser {
+public:
+  static json parse(string str);
+};
 }
 
 #endif
